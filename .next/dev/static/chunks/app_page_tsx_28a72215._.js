665b7fc20c7286eb5a1d3893fbd2bb789f6b0561
sergioortizlatorre2 <sergioortizlatorre@gmail.com>
2026-02-26 20:26:57 -0300

    importante
    importante

---
+++ .next/dev/static/chunks/app_page_tsx_28a72215._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_92c0532e._.js",
+  "static/chunks/node_modules__pnpm_d446105f._.js"
+],
+    source: "dynamic"
+});
